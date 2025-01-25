fcd1701843844628bdd4b61ab01990c413d52824
GinoPoitier <gino.poitier@gmail.com>
2025-01-25 14:06:04 +0100

    Init upgrade to newer version of template

---
+++ data/projectsData.ts
@@ -0,0 +1,20 @@
+interface Project {
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'Horizon: Zero Dawn - Tallneck model',
+    description: `Horizon: Zero Dawn has been captivating my imagination recently, it's world building and scenery alone is just amazing.
+    So since i was looking for a new 3d printing project, this model
+    by Gloomeskk and remixed by blarbles over at thingiverse was just perfect. Let the day long prints begin!`,
+    imgSrc: '/static/images/tallneck/example_result.jpg',
+    href: '/blog/Tallneck-model/horizon-zd-tallneck-1',
+  }
+]
+
+
+export default projectsData
